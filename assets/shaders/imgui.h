577858d9de46f2c68226ab1d75f99dd737897dbc
AdamFull <logotipick@gmail.com>
2025-10-02 13:32:08 +0200

    Started working on shaders

---
+++ assets/shaders/imgui.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "interop.h"
+
+namespace edge::gfx::imgui {
+	struct Vertex {
+		float2 pos;
+		float2 uv;
+		uint8_t4 col;
+	};
+
+	struct PushConstant {
+		TYPE_PTR(Vertex) vertices;
+		float2 scale;
+		float2 translate;
+	};
+}
