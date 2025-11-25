a08819c9efa564e55dd4d4b1a58f7425442ee864
AdamFull <logotipick@gmail.com>
2025-11-25 23:35:54 +0100

    Instance creation

--- engine/gfx_context.c
+++ engine/gfx_context.c
@@ -2,6 +2,7 @@
 #include "runtime/platform.h"
 
 #include <edge_allocator.h>
+#include <edge_vector.h>
 #include <edge_logger.h>
 
 #include <volk.h>
@@ -19,13 +20,124 @@
 #endif
 #endif
 
+#define GFX_ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+struct gfx_key_value {
+	const char* key;
+	bool required;
+};
+
+static const char* g_instance_layers[] = {
+#if defined(USE_VALIDATION_LAYERS)
+	"VK_LAYER_KHRONOS_validation",
+#if defined(EDGE_VK_USE_SYNCHRONIZATION_VALIDATION)
+	"VK_LAYER_KHRONOS_synchronization2"
+#endif
+#endif
+};
+
+static VkValidationFeatureEnableEXT g_validation_features_enable[] = {
+#if defined(USE_VALIDATION_LAYER_FEATURES)
+	VK_VALIDATION_FEATURE_ENABLE_DEBUG_PRINTF_EXT,
+#if defined(EDGE_VK_USE_GPU_ASSISTED_VALIDATION)
+	VK_VALIDATION_FEATURE_ENABLE_GPU_ASSISTED_RESERVE_BINDING_SLOT_EXT,
+	VK_VALIDATION_FEATURE_ENABLE_GPU_ASSISTED_EXT,
+#endif
+#if defined(EDGE_VK_USE_BEST_PRACTICES_VALIDATION)
+	VK_VALIDATION_FEATURE_ENABLE_BEST_PRACTICES_EXT,
+#endif
+#if defined(EDGE_VK_USE_SYNCHRONIZATION_VALIDATION)
+	VK_VALIDATION_FEATURE_ENABLE_SYNCHRONIZATION_VALIDATION_EXT
+#endif
+#endif
+};
+
+static const char* g_instance_extensions[] = {
+	VK_KHR_SURFACE_EXTENSION_NAME, 
+#if defined(VK_USE_PLATFORM_WIN32_KHR)
+	VK_KHR_WIN32_SURFACE_EXTENSION_NAME, 
+#elif defined(VK_USE_PLATFORM_XLIB_KHR)
+	VK_KHR_XLIB_SURFACE_EXTENSION_NAME, 
+#elif defined(VK_USE_PLATFORM_XCB_KHR)
+	VK_KHR_XCB_SURFACE_EXTENSION_NAME, 
+#elif defined(VK_USE_PLATFORM_WAYLAND_KHR)
+	VK_KHR_WAYLAND_SURFACE_EXTENSION_NAME, 
+#elif defined(VK_USE_PLATFORM_ANDROID_KHR)
+	VK_KHR_ANDROID_SURFACE_EXTENSION_NAME, 
+#elif defined(VK_USE_PLATFORM_MACOS_MVK)
+	VK_MVK_MACOS_SURFACE_EXTENSION_NAME, 
+#elif defined(VK_USE_PLATFORM_IOS_MVK)
+	VK_MVK_IOS_SURFACE_EXTENSION_NAME, 
+#elif defined(VK_USE_PLATFORM_METAL_EXT)
+	VK_EXT_METAL_SURFACE_EXTENSION_NAME, 
+#else
+	VK_EXT_HEADLESS_SURFACE_EXTENSION_NAME, 
+#endif
+#if EDGE_DEBUG
+	VK_EXT_DEBUG_UTILS_EXTENSION_NAME
+#endif // EDGE_DEBUG
+};
+
+static struct gfx_key_value g_device_extensions[] = {
+	{ VK_KHR_SWAPCHAIN_EXTENSION_NAME, true },
+	{ VK_KHR_TIMELINE_SEMAPHORE_EXTENSION_NAME, true },
+	{ VK_KHR_MAINTENANCE_4_EXTENSION_NAME, true },
+	{ VK_KHR_CREATE_RENDERPASS_2_EXTENSION_NAME, true },
+	{ VK_KHR_DEPTH_STENCIL_RESOLVE_EXTENSION_NAME, true },
+	{ VK_EXT_DESCRIPTOR_INDEXING_EXTENSION_NAME, true },
+	{ VK_KHR_SHADER_DRAW_PARAMETERS_EXTENSION_NAME, true },
+	{ VK_KHR_SHADER_FLOAT16_INT8_EXTENSION_NAME, true },
+	{ VK_KHR_8BIT_STORAGE_EXTENSION_NAME, true },
+	{ VK_KHR_16BIT_STORAGE_EXTENSION_NAME, true },
+	{ VK_KHR_DRAW_INDIRECT_COUNT_EXTENSION_NAME, true },
+	{ VK_KHR_SHADER_FLOAT_CONTROLS_EXTENSION_NAME, true },
+	{ VK_KHR_SPIRV_1_4_EXTENSION_NAME, true },
+	{ VK_KHR_SEPARATE_DEPTH_STENCIL_LAYOUTS_EXTENSION_NAME, true },
+	{ VK_KHR_COPY_COMMANDS_2_EXTENSION_NAME, true },
+	{ VK_EXT_EXTENDED_DYNAMIC_STATE_EXTENSION_NAME, true },
+	{ VK_KHR_SHADER_NON_SEMANTIC_INFO_EXTENSION_NAME, true },
+	{ VK_KHR_VULKAN_MEMORY_MODEL_EXTENSION_NAME, true },
+	{ VK_EXT_MEMORY_BUDGET_EXTENSION_NAME, false },
+	{ VK_KHR_GET_MEMORY_REQUIREMENTS_2_EXTENSION_NAME, false },
+	{ VK_KHR_DEDICATED_ALLOCATION_EXTENSION_NAME, false },
+	{ VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME, false },
+	{ VK_KHR_PERFORMANCE_QUERY_EXTENSION_NAME, false },
+	{ VK_EXT_HOST_QUERY_RESET_EXTENSION_NAME, false },
+	{ VK_EXT_MEMORY_PRIORITY_EXTENSION_NAME, false },
+	{ VK_KHR_BIND_MEMORY_2_EXTENSION_NAME, false },
+	{ VK_AMD_DEVICE_COHERENT_MEMORY_EXTENSION_NAME, false },
+	{ VK_KHR_SYNCHRONIZATION_2_EXTENSION_NAME, true },
+	{ VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME, true },
+	{ VK_EXT_EXTENDED_DYNAMIC_STATE_EXTENSION_NAME, true },
+#if USE_NSIGHT_AFTERMATH
+	{ VK_NV_DEVICE_DIAGNOSTIC_CHECKPOINTS_EXTENSION_NAME, false },
+	{ VK_NV_DEVICE_DIAGNOSTICS_CONFIG_EXTENSION_NAME, false },
+#endif
+};
+
+struct gfx_instance {
+	const edge_allocator_t* alloc;
+	const VkAllocationCallbacks* alloc_cb;
+
+	VkInstance handle;
+	VkDebugUtilsMessengerEXT debug_messenger;
+
+	const char* enabled_layers[32];
+	uint32_t enabled_layer_count;
+
+	const char* enabled_extensions[32];
+	uint32_t enabled_extension_count;
+
+	bool validation_enabled;
+	bool synchronization_validation_enabled;
+};
+
 struct gfx_context {
 	const edge_allocator_t* alloc;
 	VkAllocationCallbacks alloc_callbacks;
-	VkResult volk_result;
 
-	VkInstance inst;
-	VkDebugUtilsMessengerEXT debug_messenger;
+	struct gfx_instance* instance;
+
 	VkSurfaceKHR surf;
 };
 
@@ -80,75 +192,192 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debug_utils_messenger_cb(
 	return VK_FALSE;
 }
 
-static bool gfx_context_init_instance(gfx_context_t* ctx) {
-	if (!ctx) {
-		return false;
+//#if defined(VK_USE_PLATFORM_WIN32_KHR)
+//	VkWin32SurfaceCreateInfoKHR create_info;
+//	platform_context_get_surface(platform_ctx, &create_info);
+//
+//	VkResult result = vkCreateWin32SurfaceKHR(ctx->inst, &create_info, &ctx->alloc_callbacks, &ctx->surf);
+//	if (result != VK_SUCCESS) {
+//		return false;
+//	}
+//#elif defined(VK_USE_PLATFORM_XLIB_KHR)
+//#elif defined(VK_USE_PLATFORM_ANDROID_KHR)
+//#endif
+
+static bool is_extension_supported(const char* extension_name, const VkExtensionProperties* available_extensions, uint32_t available_count) {
+	for (int32_t i = 0; i < available_count; i++) {
+		if (strcmp(extension_name, available_extensions[i].extensionName) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+static bool is_layer_supported(const char* layer_name, const VkLayerProperties* available_layers, uint32_t available_count) {
+	for (int32_t i = 0; i < available_count; i++) {
+		if (strcmp(layer_name, available_layers[i].layerName) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+static struct gfx_instance* gfx_instance_create(gfx_context_t* ctx) {
+	if (!ctx || !ctx->alloc) {
+		return NULL;
 	}
 
-	const char* layers[] = {
-#if defined(USE_VALIDATION_LAYERS)
-		"VK_LAYER_KHRONOS_validation"
+	const edge_allocator_t* alloc = ctx->alloc;
+
+	struct gfx_instance* inst = (struct gfx_instance*)edge_allocator_malloc(alloc, sizeof(struct gfx_instance));
+	if (!inst) {
+		return NULL;
+	}
+
+	inst->alloc = alloc;
+	inst->alloc_cb = &ctx->alloc_callbacks;
+	inst->handle = VK_NULL_HANDLE;
+	inst->debug_messenger = VK_NULL_HANDLE;
+	inst->enabled_layer_count = 0;
+	inst->enabled_extension_count = 0;
+	inst->validation_enabled = false;
+	inst->synchronization_validation_enabled = false;
+
+	uint32_t layer_count = 0;
+	vkEnumerateInstanceLayerProperties(&layer_count, NULL);
+	VkLayerProperties* available_layers = NULL;
+	if (layer_count > 0) {
+		available_layers = (VkLayerProperties*)edge_allocator_malloc(alloc, layer_count * sizeof(VkLayerProperties));
+		vkEnumerateInstanceLayerProperties(&layer_count, available_layers);
+	}
+
+	uint32_t extension_count = 0;
+	vkEnumerateInstanceExtensionProperties(NULL, &extension_count, NULL);
+	VkExtensionProperties* available_extensions = NULL;
+	if (extension_count > 0) {
+		available_extensions = (VkExtensionProperties*)edge_allocator_malloc(alloc, extension_count * sizeof(VkExtensionProperties));
+		vkEnumerateInstanceExtensionProperties(NULL, &extension_count, available_extensions);
+	}
+	
+	for (int i = 0; i < GFX_ARRAY_SIZE(g_instance_layers); ++i) {
+		const char* layer_name = g_instance_layers[i];
+		if (is_layer_supported(layer_name, available_layers, layer_count)) {
+			inst->enabled_layers[inst->enabled_layer_count++] = layer_name;
+#ifdef USE_VALIDATION_LAYERS
+			if (strcmp(layer_name, "VK_LAYER_KHRONOS_validation") == 0) {
+				inst->validation_enabled = true;
+			}
+			else if (strcmp(layer_name, "VK_LAYER_KHRONOS_synchronization2") == 0) {
+				inst->synchronization_validation_enabled = true;
+			}
 #endif
-	};
-	uint32_t layer_count = sizeof(layers) / sizeof(layers[0]);
+		}
+		else {
+			EDGE_LOG_WARN("Layer not supported: %s", layer_name);
+		}
+	}
 
-	const char* extensions[] = {
-		VK_KHR_SURFACE_EXTENSION_NAME,
-#if defined(VK_USE_PLATFORM_WIN32_KHR)
-		VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
-#elif defined(VK_USE_PLATFORM_XLIB_KHR)
-		VK_KHR_XLIB_SURFACE_EXTENSION_NAME,
-#elif defined(VK_USE_PLATFORM_ANDROID_KHR)
-		VK_KHR_ANDROID_SURFACE_EXTENSION_NAME,
+	for (int i = 0; i < GFX_ARRAY_SIZE(g_instance_extensions); ++i) {
+		const char* ext_name = g_instance_extensions[i];
+		if (is_extension_supported(ext_name, available_extensions, extension_count)) {
+			inst->enabled_extensions[inst->enabled_extension_count++] = ext_name;
+		}
+		else {
+			EDGE_LOG_WARN("Required instance extension not supported: %s\n", ext_name);
+		}
+	}
+
+	if (available_layers) {
+		edge_allocator_free(alloc, available_layers);
+	}
+
+	if (available_extensions) {
+		edge_allocator_free(alloc, available_extensions);
+	}
+
+	VkApplicationInfo app_info = { 0 };
+	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	app_info.pApplicationName = "applicationname";
+	app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0); // TODO: Generate
+	app_info.pEngineName = "enginename";
+	app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0); // TODO: Generate
+	app_info.apiVersion = VK_API_VERSION_1_1;
+
+	VkInstanceCreateInfo instance_info = { 0 };
+	instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instance_info.pApplicationInfo = &app_info;
+	instance_info.enabledLayerCount = inst->enabled_layer_count;
+	instance_info.ppEnabledLayerNames = inst->enabled_layers;
+	instance_info.enabledExtensionCount = inst->enabled_extension_count;
+	instance_info.ppEnabledExtensionNames = inst->enabled_extensions;
+
+#ifdef USE_VALIDATION_LAYER_FEATURES
+	VkValidationFeaturesEXT validation_features = { 0 };
+	if (inst->validation_enabled) {
+		validation_features.sType = VK_STRUCTURE_TYPE_VALIDATION_FEATURES_EXT;
+		validation_features.enabledValidationFeatureCount = GFX_ARRAY_SIZE(g_validation_features_enable);
+		validation_features.pEnabledValidationFeatures = g_validation_features_enable;
+		instance_info.pNext = &validation_features;
+	}
 #endif
-		VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME,
-#if defined(EDGE_DEBUG)
-		VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
-#endif
-	};
-	uint32_t extension_count = sizeof(extensions) / sizeof(extensions[0]);
 
-	VkApplicationInfo application_info;
-	application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	application_info.pNext = NULL;
-	application_info.pApplicationName = "applicationname";
-	application_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-	application_info.pEngineName = "enginename";
-	application_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-	application_info.apiVersion = VK_MAKE_VERSION(1, 1, 0);
-
-	VkInstanceCreateInfo create_info;
-	create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	create_info.pNext = NULL;
-	create_info.flags = 0;
-	create_info.pApplicationInfo = &application_info;
-	create_info.enabledLayerCount = layer_count;
-	create_info.ppEnabledLayerNames = layers;
-	create_info.enabledExtensionCount = extension_count;
-	create_info.ppEnabledExtensionNames = extensions;
-
-	VkResult result = vkCreateInstance(&create_info, &ctx->alloc_callbacks, &ctx->inst);
+	VkResult result = vkCreateInstance(&instance_info, &ctx->alloc_callbacks, &inst->handle);
 	if (result != VK_SUCCESS) {
-		return false;
+		EDGE_LOG_FATAL("Failed to create Vulkan instance: %d\n", result);
+		goto fatal_error;
 	}
 
-	volkLoadInstance(ctx->inst);
+	volkLoadInstance(inst->handle);
 
-#if defined(EDGE_DEBUG)
-	VkDebugUtilsMessengerCreateInfoEXT debug_create_info;
-	debug_create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-	debug_create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-	debug_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
-		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-	debug_create_info.pfnUserCallback = debug_utils_messenger_cb;
+#ifdef USE_VALIDATION_LAYERS
+	if (inst->validation_enabled) {
+		VkDebugUtilsMessengerCreateInfoEXT debug_info = { 0 };
+		debug_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		debug_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		debug_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		debug_info.pfnUserCallback = debug_utils_messenger_cb;
 
-	result = vkCreateDebugUtilsMessengerEXT(ctx->inst, &debug_create_info, &ctx->alloc_callbacks, &ctx->debug_messenger);
-	if (result != VK_SUCCESS) {
-		return false;
+		result = vkCreateDebugUtilsMessengerEXT(inst->handle, &debug_info, &ctx->alloc_callbacks, &inst->debug_messenger);
+		if (result != VK_SUCCESS) {
+			EDGE_LOG_WARN("Failed to create debug messenger: %d", result);
+		}
 	}
 #endif
 
-	return true;
+	return inst;
+
+fatal_error:
+	if (available_layers) {
+		edge_allocator_free(alloc, available_layers);
+	}
+
+	if (available_extensions) {
+		edge_allocator_free(alloc, available_extensions);
+	}
+
+	if (inst) {
+		edge_allocator_free(alloc, inst);
+	}
+
+	return NULL;
+}
+
+static void gfx_instance_destroy(struct gfx_instance* inst) {
+	if (!inst || !inst->alloc || !inst->alloc_cb) {
+		return;
+	}
+
+	if (inst->debug_messenger != VK_NULL_HANDLE) {
+		vkDestroyDebugUtilsMessengerEXT(inst->handle, inst->debug_messenger, inst->alloc_cb);
+	}
+
+	if (inst->handle != VK_NULL_HANDLE) {
+		vkDestroyInstance(inst->handle, inst->alloc_cb);
+	}
+
+	const edge_allocator_t* alloc = inst->alloc;
+	edge_allocator_free(alloc, inst);
 }
 
 gfx_context_t* gfx_context_create(const edge_allocator_t* alloc, platform_context_t* platform_ctx) {
@@ -156,9 +385,15 @@ gfx_context_t* gfx_context_create(const edge_allocator_t* alloc, platform_contex
 		return NULL;
 	}
 
+	VkResult result = volkInitialize();
+	if (result != VK_SUCCESS) {
+		EDGE_LOG_ERROR("Failed to initialize volk: %d", result);
+		return NULL;
+	}
+
 	gfx_context_t* ctx = (gfx_context_t*)edge_allocator_calloc(alloc, 1, sizeof(gfx_context_t));
 	if (!ctx) {
-		return NULL;
+		goto fatal_error;
 	}
 
 	ctx->alloc = alloc;
@@ -171,29 +406,17 @@ gfx_context_t* gfx_context_create(const edge_allocator_t* alloc, platform_contex
 	ctx->alloc_callbacks.pfnInternalAllocation = vk_internal_alloc_cb;
 	ctx->alloc_callbacks.pfnInternalFree = vk_internal_free_cb;
 
-	// Init volk
-	ctx->volk_result = (VkResult)-1;
-	ctx->volk_result = volkInitialize();
-
-	if (!gfx_context_init_instance(ctx)) {
+	ctx->instance = gfx_instance_create(ctx);
+	if (!ctx->instance) {
 		gfx_context_destroy(ctx);
 		return NULL;
 	}
-
-#if defined(VK_USE_PLATFORM_WIN32_KHR)
-	VkWin32SurfaceCreateInfoKHR surface_create_info;
-	platform_context_get_surface(platform_ctx, &surface_create_info);
-
-	VkResult result = vkCreateWin32SurfaceKHR(ctx->inst, &surface_create_info, &ctx->alloc_callbacks, &ctx->surf);
-	if (result != VK_SUCCESS) {
-		gfx_context_destroy(ctx);
-		return NULL;
-	}
-#elif defined(VK_USE_PLATFORM_XLIB_KHR)
-#elif defined(VK_USE_PLATFORM_ANDROID_KHR)
-#endif
 
 	return ctx;
+
+fatal_error:
+	gfx_context_destroy(ctx);
+	return NULL;
 }
 
 void gfx_context_destroy(gfx_context_t* ctx) {
@@ -201,24 +424,12 @@ void gfx_context_destroy(gfx_context_t* ctx) {
 		return;
 	}
 
-	if (ctx->surf != VK_NULL_HANDLE) {
-		vkDestroySurfaceKHR(ctx->inst, ctx->surf, &ctx->alloc_callbacks);
-	}
-
-#if defined(EDGE_DEBUG)
-	if (ctx->debug_messenger != VK_NULL_HANDLE) {
-		vkDestroyDebugUtilsMessengerEXT(ctx->inst, ctx->debug_messenger, &ctx->alloc_callbacks);
-	}
-#endif
-
-	if (ctx->inst != VK_NULL_HANDLE) {
-		vkDestroyInstance(ctx->inst, &ctx->alloc_callbacks);
-	}
-
-	if (ctx->volk_result == VK_SUCCESS) {
-		volkFinalize();
+	if (ctx->instance) {
+		gfx_instance_destroy(ctx->instance);
 	}
 
 	const edge_allocator_t* alloc = ctx->alloc;
 	edge_allocator_free(alloc, ctx);
+
+	volkFinalize();
 }
