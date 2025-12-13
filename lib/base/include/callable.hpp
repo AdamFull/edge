cced01a9d820188db9bba64c4272a463bb5108c8
AdamFull <logotipick@gmail.com>
2025-12-13 01:35:02 +0100

    Create callable.hpp

---
+++ lib/base/include/callable.hpp
@@ -0,0 +1,134 @@
+#ifndef EDGE_CALLABLE_H
+#define EDGE_CALLABLE_H
+
+#include <utility>
+#include <type_traits>
+
+#include "allocator.hpp"
+
+namespace edge {
+	template<typename>
+	struct Callable;
+
+	template<typename R, typename... Args>
+	struct Callable<R(Args...)> {
+		R(*invoke_fn)(void* data, Args... args);
+		void* data;
+	};
+
+	template<typename T>
+	struct callable_traits : callable_traits<decltype(&T::operator())> {};
+
+	template<typename C, typename R, typename... Args>
+	struct callable_traits<R(C::*)(Args...) const> {
+		using signature = R(Args...);
+		using return_type = R;
+	};
+
+	template<typename C, typename R, typename... Args>
+	struct callable_traits<R(C::*)(Args...)> {
+		using signature = R(Args...);
+		using return_type = R;
+	};
+
+	template<typename R, typename... Args>
+	struct callable_traits<R(*)(Args...)> {
+		using signature = R(Args...);
+		using return_type = R;
+	};
+
+	template<typename R, typename... Args>
+	inline Callable<R(Args...)> callable_create_from_func(const Allocator* alloc, R(*fn)(Args...)) {
+		using FnPtr = R(*)(Args...);
+
+		FnPtr* stored = allocate<FnPtr>(alloc);
+		*stored = fn;
+
+		Callable<R(Args...)> result;
+		result.invoke_fn = [](void* data, Args... args) -> R {
+			FnPtr fn_ptr = *static_cast<FnPtr*>(data);
+			return fn_ptr(std::forward<Args>(args)...);
+			};
+		result.data = stored;
+		return result;
+	}
+
+	template<typename F>
+	inline auto callable_create_from_lambda(const Allocator* alloc, F&& functor) {
+		using FType = std::decay_t<F>;
+		using Sig = typename callable_traits<FType>::signature;
+
+		FType* stored = allocate<FType>(alloc);
+		new (stored) FType(std::forward<F>(functor));
+
+		return[=]<typename R, typename... Args>(R(*)(Args...)) {
+			Callable<R(Args...)> result;
+			result.invoke_fn = [](void* data, Args... args) -> R {
+				FType* fn = static_cast<FType*>(data);
+				return (*fn)(std::forward<Args>(args)...);
+				};
+			result.data = stored;
+			return result;
+		}(static_cast<Sig*>(nullptr));
+	}
+
+	template<typename C, typename R, typename... Args>
+	inline Callable<R(Args...)> callable_create_from_member(const Allocator* alloc, R(C::* fn)(Args...), C* instance) {
+		struct MemberData {
+			R(C::* fn)(Args...);
+			C* instance;
+		};
+
+		MemberData* stored = allocate<MemberData>(alloc);
+		stored->fn = fn;
+		stored->instance = instance;
+
+		Callable<R(Args...)> result;
+		result.invoke_fn = [](void* data, Args... args) -> R {
+			MemberData* md = static_cast<MemberData*>(data);
+			return (md->instance->*(md->fn))(std::forward<Args>(args)...);
+			};
+		result.data = stored;
+		return result;
+	}
+
+	template<typename C, typename R, typename... Args>
+	inline Callable<R(Args...)> callable_create_from_const_member(const Allocator* alloc, R(C::* fn)(Args...) const, const C* instance) {
+		struct MemberData {
+			R(C::* fn)(Args...) const;
+			const C* instance;
+		};
+
+		MemberData* stored = allocate<MemberData>(alloc);
+		stored->fn = fn;
+		stored->instance = instance;
+
+		Callable<R(Args...)> result;
+		result.invoke_fn = [](void* data, Args... args) -> R {
+			MemberData* md = static_cast<MemberData*>(data);
+			return (md->instance->*(md->fn))(std::forward<Args>(args)...);
+			};
+		result.data = stored;
+		return result;
+	}
+
+	template<typename R, typename... Args>
+	inline R callable_invoke(Callable<R(Args...)>* callable, Args... args) {
+		return callable->invoke_fn(callable->data, std::forward<Args>(args)...);
+	}
+
+	template<typename R, typename... Args>
+	inline void callable_destroy(const Allocator* alloc, Callable<R(Args...)>* callable) {
+		if (alloc && callable->data) {
+			deallocate(alloc, callable->data);
+		}
+		callable->data = nullptr;
+	}
+
+	template<typename R, typename... Args>
+	inline bool callable_is_valid(const Callable<R(Args...)>* callable) {
+		return callable && callable->invoke_fn != nullptr;
+	}
+}
+
+#endif
