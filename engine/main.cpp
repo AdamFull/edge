0053c83fab7c96fe3705b2ae8f9ab639a025fb7b
AdamFull <logotipick@gmail.com>
2025-12-20 12:53:14 +0100

    Update main.cpp

--- engine/main.cpp
+++ engine/main.cpp
@@ -90,12 +90,13 @@ int edge_main(edge::PlatformLayout* platform_layout) {
 		return -1;
 	}
 
-	edge::PlatformContextCreateInfo platform_context_create_info = {};
-	platform_context_create_info.alloc = &allocator;
-	platform_context_create_info.layout = platform_layout;
-	platform_context_create_info.event_dispatcher = engine_context.event_dispatcher;
+	const edge::PlatformContextCreateInfo platform_context_create_info = {
+		.alloc = &allocator,
+		.layout = platform_layout,
+		.event_dispatcher = engine_context.event_dispatcher
+	};
 
-	engine_context.platform_context = platform_context_create(&platform_context_create_info);
+	engine_context.platform_context = platform_context_create(platform_context_create_info);
 	if (!engine_context.platform_context) {
 		edge_cleanup_engine();
 		return -1;
@@ -104,42 +105,46 @@ int edge_main(edge::PlatformLayout* platform_layout) {
 	EDGE_LOG_INFO("Context initialization finished.");
 	edge::logger_flush(edge::logger_get_global());
 
-	edge::WindowCreateInfo window_create_info = {};
-	window_create_info.title = "Window";
-	window_create_info.mode = edge::WindowMode::Default;
-	window_create_info.resizable = true;
-	window_create_info.vsync_mode = edge::WindowVsyncMode::Off;
-	window_create_info.width = 1280;
-	window_create_info.height = 720;
+	const edge::WindowCreateInfo window_create_info = {
+		.title = "Window",
+		.mode = edge::WindowMode::Default,
+		.resizable = true,
+		.vsync_mode = edge::WindowVsyncMode::Off,
+		.width = 1280,
+		.height = 720
+	};
 
-	if (!platform_context_window_init(engine_context.platform_context, &window_create_info)) {
+	if (!platform_context_window_init(engine_context.platform_context, window_create_info)) {
 		edge_cleanup_engine();
 		return -1;
 	}
 
-	edge::gfx::ContextCreateInfo gfx_cteate_info = {};
-	gfx_cteate_info.alloc = &allocator;
-	gfx_cteate_info.platform_context = engine_context.platform_context;
+	const edge::gfx::ContextCreateInfo gfx_cteate_info = {
+		.alloc = &allocator,
+		.platform_context = engine_context.platform_context
+	};
 
 	if (!edge::gfx::context_init(&gfx_cteate_info)) {
 		edge_cleanup_engine();
 		return -1;
 	}
 
-	edge::gfx::QueueRequest queue_request;
-	queue_request.required_caps = edge::gfx::QUEUE_CAPS_GRAPHICS | edge::gfx::QUEUE_CAPS_COMPUTE | edge::gfx::QUEUE_CAPS_TRANSFER | edge::gfx::QUEUE_CAPS_PRESENT;
-	queue_request.preferred_caps = edge::gfx::QUEUE_CAPS_NONE;
-	queue_request.strategy = edge::gfx::QUEUE_SELECTION_STRATEGY_PREFER_DEDICATED;
-	queue_request.prefer_separate_family = false;
+	const edge::gfx::QueueRequest queue_request = {
+		.required_caps = edge::gfx::QUEUE_CAPS_GRAPHICS | edge::gfx::QUEUE_CAPS_COMPUTE | edge::gfx::QUEUE_CAPS_TRANSFER | edge::gfx::QUEUE_CAPS_PRESENT,
+		.preferred_caps = edge::gfx::QUEUE_CAPS_NONE,
+		.strategy = edge::gfx::QUEUE_SELECTION_STRATEGY_PREFER_DEDICATED,
+		.prefer_separate_family = false
+	};
 
 	if (!edge::gfx::get_queue(queue_request, engine_context.main_queue)) {
 		edge_cleanup_engine();
 		return -1;
 	}
 
-	edge::gfx::RendererCreateInfo renderer_create_info;
-	renderer_create_info.alloc = &allocator;
-	renderer_create_info.main_queue = engine_context.main_queue;
+	const edge::gfx::RendererCreateInfo renderer_create_info = {
+		.alloc = &allocator,
+		.main_queue = engine_context.main_queue
+	};
 
 	engine_context.renderer = edge::gfx::renderer_create(renderer_create_info);
 	if (!engine_context.renderer) {
@@ -147,7 +152,7 @@ int edge_main(edge::PlatformLayout* platform_layout) {
 		return -1;
 	}
 
-	edge::ImGuiLayerInitInfo imgui_init_info = {
+	const edge::ImGuiLayerInitInfo imgui_init_info = {
 		.alocator = engine_context.allocator,
 		.event_dispatcher = engine_context.event_dispatcher,
 		.platform_context = engine_context.platform_context
@@ -159,12 +164,12 @@ int edge_main(edge::PlatformLayout* platform_layout) {
 		return -1;
 	}
 
-	edge::gfx::ImGuiRendererCreateInfo imgui_renderer_create_info = {
+	const edge::gfx::ImGuiRendererCreateInfo imgui_renderer_create_info = {
 		.allocator = engine_context.allocator,
 		.renderer = engine_context.renderer
 	};
 
-	engine_context.imgui_renderer = edge::gfx::imgui_renderer_create(&imgui_renderer_create_info);
+	engine_context.imgui_renderer = edge::gfx::imgui_renderer_create(imgui_renderer_create_info);
 	if (!engine_context.imgui_renderer) {
 		edge_cleanup_engine();
 		return -1;
