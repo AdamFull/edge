d884419e8875d22530ea8136c6a0bb904bdf3e0b
AdamFull <logotipick@gmail.com>
2025-12-20 18:54:14 +0100

    Update main.cpp

--- engine/main.cpp
+++ engine/main.cpp
@@ -15,24 +15,26 @@
 #include <assert.h>
 #include <mimalloc.h>
 
-static edge::EngineContext engine_context = {};
+using namespace edge;
+
+static EngineContext engine_context = {};
 
 static void edge_cleanup_engine(void) {
 	if (engine_context.imgui_layer) {
-		edge::imgui_layer_destroy(engine_context.imgui_layer);
+		imgui_layer_destroy(engine_context.imgui_layer);
 	}
 
 	if (engine_context.imgui_renderer) {
-		edge::gfx::imgui_renderer_destroy(engine_context.imgui_renderer);
+		gfx::imgui_renderer_destroy(engine_context.imgui_renderer);
 	}
 
 	if (engine_context.renderer) {
-		edge::gfx::renderer_destroy(engine_context.renderer);
+		gfx::renderer_destroy(engine_context.renderer);
 	}
 
-	edge::gfx::release_queue(engine_context.main_queue);
+	gfx::release_queue(engine_context.main_queue);
 	
-	edge::gfx::context_shutdown();
+	gfx::context_shutdown();
 
 	if (engine_context.event_dispatcher) {
 		event_dispatcher_destroy(engine_context.event_dispatcher);
@@ -43,42 +45,42 @@ static void edge_cleanup_engine(void) {
 	}
 
 	if (engine_context.sched) {
-		edge::sched_destroy(engine_context.sched);
+		sched_destroy(engine_context.sched);
 	}
 
 	if (engine_context.logger) {
-		edge::logger_destroy(engine_context.logger);
+		logger_destroy(engine_context.logger);
 	}
 
 	size_t net_allocated = engine_context.allocator->get_net();
 	assert(net_allocated == 0 && "Memory leaks detected.");
 }
 
-int edge_main(edge::PlatformLayout* platform_layout) {
+int edge_main(PlatformLayout* platform_layout) {
 #if EDGE_DEBUG
-	edge::Allocator allocator = edge::Allocator::create_tracking();
+	Allocator allocator = Allocator::create_tracking();
 #else
-	edge::Allocator allocator = edge::Allocator::create(mi_malloc, mi_free, mi_realloc, mi_calloc, mi_strdup);
+	Allocator allocator = Allocator::create(mi_malloc, mi_free, mi_realloc, mi_calloc, mi_strdup);
 #endif
 
 	engine_context.allocator = &allocator;
 	engine_context.platform_layout = platform_layout;
 
-	engine_context.logger = edge::logger_create(&allocator, edge::LogLevel::Trace);
+	engine_context.logger = logger_create(&allocator, LogLevel::Trace);
 	if (!engine_context.logger) {
 		edge_cleanup_engine();
 		return -1;
 	}
 
-	edge::logger_set_global(engine_context.logger);
+	logger_set_global(engine_context.logger);
 
-	edge::LoggerOutput* stdout_output = edge::logger_create_stdout_output(&allocator, edge::LogFormat_Default | edge::LogFormat_Color);
-	edge::logger_add_output(engine_context.logger, stdout_output);
+	LoggerOutput* stdout_output = logger_create_stdout_output(&allocator, LogFormat_Default | LogFormat_Color);
+	logger_add_output(engine_context.logger, stdout_output);
 
-	edge::LoggerOutput* file_output = edge::logger_create_file_output(&allocator, edge::LogFormat_Default, "log.log", false);
-	edge::logger_add_output(engine_context.logger, file_output);
+	LoggerOutput* file_output = logger_create_file_output(&allocator, LogFormat_Default, "log.log", false);
+	logger_add_output(engine_context.logger, file_output);
 
-	engine_context.sched = edge::sched_create(&allocator);
+	engine_context.sched = sched_create(&allocator);
 	if (!engine_context.sched) {
 		edge_cleanup_engine();
 		return -1;
@@ -90,7 +92,7 @@ int edge_main(edge::PlatformLayout* platform_layout) {
 		return -1;
 	}
 
-	const edge::PlatformContextCreateInfo platform_context_create_info = {
+	const PlatformContextCreateInfo platform_context_create_info = {
 		.alloc = &allocator,
 		.layout = platform_layout,
 		.event_dispatcher = engine_context.event_dispatcher
@@ -103,13 +105,13 @@ int edge_main(edge::PlatformLayout* platform_layout) {
 	}
 
 	EDGE_LOG_INFO("Context initialization finished.");
-	edge::logger_flush(edge::logger_get_global());
+	logger_flush(logger_get_global());
 
-	const edge::WindowCreateInfo window_create_info = {
+	const WindowCreateInfo window_create_info = {
 		.title = "Window",
-		.mode = edge::WindowMode::Default,
+		.mode = WindowMode::Default,
 		.resizable = true,
-		.vsync_mode = edge::WindowVsyncMode::Off,
+		.vsync_mode = WindowVsyncMode::Off,
 		.width = 1280,
 		.height = 720
 	};
@@ -119,57 +121,57 @@ int edge_main(edge::PlatformLayout* platform_layout) {
 		return -1;
 	}
 
-	const edge::gfx::ContextCreateInfo gfx_cteate_info = {
+	const gfx::ContextCreateInfo gfx_cteate_info = {
 		.alloc = &allocator,
 		.platform_context = engine_context.platform_context
 	};
 
-	if (!edge::gfx::context_init(&gfx_cteate_info)) {
+	if (!gfx::context_init(&gfx_cteate_info)) {
 		edge_cleanup_engine();
 		return -1;
 	}
 
-	const edge::gfx::QueueRequest queue_request = {
-		.required_caps = edge::gfx::QUEUE_CAPS_GRAPHICS | edge::gfx::QUEUE_CAPS_COMPUTE | edge::gfx::QUEUE_CAPS_TRANSFER | edge::gfx::QUEUE_CAPS_PRESENT,
-		.preferred_caps = edge::gfx::QUEUE_CAPS_NONE,
-		.strategy = edge::gfx::QUEUE_SELECTION_STRATEGY_PREFER_DEDICATED,
+	const gfx::QueueRequest queue_request = {
+		.required_caps = gfx::QUEUE_CAPS_GRAPHICS | gfx::QUEUE_CAPS_COMPUTE | gfx::QUEUE_CAPS_TRANSFER | gfx::QUEUE_CAPS_PRESENT,
+		.preferred_caps = gfx::QUEUE_CAPS_NONE,
+		.strategy = gfx::QUEUE_SELECTION_STRATEGY_PREFER_DEDICATED,
 		.prefer_separate_family = false
 	};
 
-	if (!edge::gfx::get_queue(queue_request, engine_context.main_queue)) {
+	if (!gfx::get_queue(queue_request, engine_context.main_queue)) {
 		edge_cleanup_engine();
 		return -1;
 	}
 
-	const edge::gfx::RendererCreateInfo renderer_create_info = {
+	const gfx::RendererCreateInfo renderer_create_info = {
 		.alloc = &allocator,
 		.main_queue = engine_context.main_queue
 	};
 
-	engine_context.renderer = edge::gfx::renderer_create(renderer_create_info);
+	engine_context.renderer = gfx::renderer_create(renderer_create_info);
 	if (!engine_context.renderer) {
 		edge_cleanup_engine();
 		return -1;
 	}
 
-	const edge::ImGuiLayerInitInfo imgui_init_info = {
+	const ImGuiLayerInitInfo imgui_init_info = {
 		.alocator = engine_context.allocator,
 		.event_dispatcher = engine_context.event_dispatcher,
 		.platform_context = engine_context.platform_context
 	};
 
-	engine_context.imgui_layer = edge::imgui_layer_create(&imgui_init_info);
+	engine_context.imgui_layer = imgui_layer_create(imgui_init_info);
 	if (!engine_context.imgui_layer) {
 		edge_cleanup_engine();
 		return -1;
 	}
 
-	const edge::gfx::ImGuiRendererCreateInfo imgui_renderer_create_info = {
+	const gfx::ImGuiRendererCreateInfo imgui_renderer_create_info = {
 		.allocator = engine_context.allocator,
 		.renderer = engine_context.renderer
 	};
 
-	engine_context.imgui_renderer = edge::gfx::imgui_renderer_create(imgui_renderer_create_info);
+	engine_context.imgui_renderer = gfx::imgui_renderer_create(imgui_renderer_create_info);
 	if (!engine_context.imgui_renderer) {
 		edge_cleanup_engine();
 		return -1;
@@ -178,11 +180,11 @@ int edge_main(edge::PlatformLayout* platform_layout) {
     while (!platform_context_window_should_close(engine_context.platform_context)) {
 		platform_context_window_process_events(engine_context.platform_context, 0.1f);
 
-		edge::imgui_layer_update(engine_context.imgui_layer, 0.1f);
+		imgui_layer_update(engine_context.imgui_layer, 0.1f);
 
-		if (edge::gfx::renderer_frame_begin(engine_context.renderer)) {
-
-			edge::gfx::renderer_frame_end(engine_context.renderer);
+		if (gfx::renderer_frame_begin(engine_context.renderer)) {
+			gfx::imgui_renderer_execute(engine_context.imgui_renderer);
+			gfx::renderer_frame_end(engine_context.renderer);
 		}
     }
 
