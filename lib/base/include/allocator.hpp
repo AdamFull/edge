73813eeb8f92083daae3beea5189ab635ec5355b
AdamFull <logotipick@gmail.com>
2025-12-24 23:20:37 +0100

    Update allocator.hpp

--- lib/base/include/allocator.hpp
+++ lib/base/include/allocator.hpp
@@ -2,6 +2,8 @@
 #define EDGE_ALLOCATOR_H
 
 #include "stddef.hpp"
+#include "platform_detect.hpp"
+
 #include <atomic>
 #include <new>
 
@@ -16,23 +18,114 @@ namespace edge {
 			usize size;
 		};
 
-		inline void* tracked_malloc(usize size, void* user_data) {
+		inline void* aligned_malloc(usize size, usize alignment) {
+			if (alignment == 0 || (alignment & (alignment - 1)) != 0) {
+				return nullptr;
+			}
+
+			if (alignment < sizeof(void*)) {
+				alignment = sizeof(void*);
+			}
+
+			void* ptr = nullptr;
+#if defined(EDGE_HAS_WINDOWS_API)
+			ptr = _aligned_malloc(size, alignment);
+#elif defined(EDGE_PLATFORM_POSIX)
+			if (posix_memalign(&ptr, alignment, size) != 0) {
+				ptr = nullptr;
+			}
+#else
+			usize total_size = size + alignment + sizeof(void*);
+
+			void* raw_ptr = malloc(total_size);
+			if (!raw_ptr) {
+				return nullptr;
+			}
+
+			uintptr_t raw_addr = reinterpret_cast<uintptr_t>(raw_ptr);
+			uintptr_t aligned_addr = (raw_addr + sizeof(void*) + alignment - 1) & ~(alignment - 1);
+
+			ptr = reinterpret_cast<void*>(aligned_addr);
+			void** stored_ptr = reinterpret_cast<void**>(aligned_addr) - 1;
+			*stored_ptr = raw_ptr;
+#endif
+
+			return ptr;
+		}
+
+		inline void aligned_free(void* ptr) {
+			if (!ptr) {
+				return;
+			}
+
+#if defined(EDGE_HAS_WINDOWS_API)
+			_aligned_free(ptr);
+#elif defined(EDGE_PLATFORM_POSIX)
+			free(ptr);
+#else
+			void** stored_ptr = reinterpret_cast<void**>(ptr) - 1;
+			void* raw_ptr = *stored_ptr;
+			free(raw_ptr);
+#endif
+		}
+
+		inline void* aligned_realloc(void* ptr, usize new_size, usize alignment) {
+			if (!ptr) {
+				return aligned_malloc(new_size, alignment);
+			}
+
+			if (new_size == 0) {
+				aligned_free(ptr);
+				return nullptr;
+			}
+
+#if defined(EDGE_HAS_WINDOWS_API)
+			return _aligned_realloc(ptr, new_size, alignment);
+#else
+			void* new_ptr = aligned_malloc(new_size, alignment);
+			if (!new_ptr) {
+				return nullptr;
+			}
+
+			memcpy(new_ptr, ptr, new_size);
+			aligned_free(ptr);
+			return new_ptr;
+#endif
+		}
+
+		inline void* tracked_malloc(usize size, usize alignment, void* user_data) {
 			if (size == 0) {
 				return nullptr;
 			}
 
-			usize total_size = sizeof(AllocationHeader) + size;
-			AllocationHeader* header = (AllocationHeader*)malloc(total_size);
-			if (!header) {
+			if (alignment < alignof(AllocationHeader)) {
+				alignment = alignof(AllocationHeader);
+			}
+
+			usize header_size = sizeof(AllocationHeader);
+			usize back_ptr_size = sizeof(void*);
+
+			usize min_offset = header_size + back_ptr_size;
+			usize user_data_offset = (min_offset + alignment - 1) & ~(alignment - 1);
+
+			usize total_size = user_data_offset + size;
+
+			usize alloc_alignment = alignment > alignof(AllocationHeader) ? alignment : alignof(AllocationHeader);
+			void* raw_ptr = aligned_malloc(total_size, alloc_alignment);
+			if (!raw_ptr) {
 				return nullptr;
 			}
 
+			AllocationHeader* header = (AllocationHeader*)raw_ptr;
 			header->size = size;
+
+			void** back_ptr = (void**)((char*)raw_ptr + user_data_offset - sizeof(void*));
+			*back_ptr = raw_ptr;
 
 			AllocatorStats* stats = (AllocatorStats*)user_data;
 			stats->alloc_bytes.fetch_add(size);
 
-			return (void*)(header + 1);
+			return (char*)raw_ptr + user_data_offset;
 		}
 
 		inline void tracked_free(void* ptr, void* user_data) {
@@ -40,17 +133,18 @@ namespace edge {
 				return;
 			}
 
-			AllocationHeader* header = ((AllocationHeader*)ptr) - 1;
+			void** back_ptr = ((void**)ptr) - 1;
+			AllocationHeader* header = (AllocationHeader*)(*back_ptr);
 
 			AllocatorStats* stats = (AllocatorStats*)user_data;
 			stats->free_bytes.fetch_add(header->size);
 
-			free(header);
+			aligned_free(header);
 		}
 
-		inline void* tracked_realloc(void* ptr, usize size, void* user_data) {
+		inline void* tracked_realloc(void* ptr, usize alignment, usize size, void* user_data) {
 			if (!ptr) {
-				return tracked_malloc(size, user_data);
+				return tracked_malloc(size, alignment, user_data);
 			}
 
 			if (size == 0) {
@@ -58,27 +152,27 @@ namespace edge {
 				return nullptr;
 			}
 
-			AllocationHeader* old_header = ((AllocationHeader*)ptr) - 1;
+			void** back_ptr = ((void**)ptr) - 1;
+			AllocationHeader* old_header = (AllocationHeader*)(*back_ptr);
 			usize old_size = old_header->size;
 
-			usize total_size = sizeof(AllocationHeader) + size;
-			AllocationHeader* new_header = (AllocationHeader*)realloc(old_header, total_size);
-			if (new_header == NULL) {
-				return NULL;
+			void* new_ptr = tracked_malloc(size, alignment, user_data);
+			if (!new_ptr) {
+				return nullptr;
 			}
 
-			AllocatorStats* stats = (AllocatorStats*)user_data;
-			stats->free_bytes.fetch_add(old_size);
-			stats->alloc_bytes.fetch_add(size);
+			usize copy_size = old_size < size ? old_size : size;
+			memcpy(new_ptr, ptr, copy_size);
 
-			new_header->size = size;
-			return (void*)(new_header + 1);
+			tracked_free(ptr, user_data);
+
+			return new_ptr;
 		}
 	}
 
-	using malloc_fn = void* (*)(usize size, void* user_data);
+	using malloc_fn = void* (*)(usize size, usize alignment, void* user_data);
 	using free_fn = void (*)(void* ptr, void* user_data);
-	using realloc_fn = void* (*)(void* ptr, usize size, void* user_data);
+	using realloc_fn = void* (*)(void* ptr, usize size, usize alignment, void* user_data);
 
 	struct Allocator {
 		malloc_fn m_malloc;
@@ -92,9 +186,9 @@ namespace edge {
 
 		static Allocator create_default() {
 			return create(
-				[](usize size, void*) { return ::malloc(size); },
-				[](void* ptr, void*) { ::free(ptr); },
-				[](void* ptr, usize size, void*) { return ::realloc(ptr, size); },
+				[](usize size, usize alignment, void*) { return detail::aligned_malloc(size, alignment); },
+				[](void* ptr, void*) { detail::aligned_free(ptr); },
+				[](void* ptr, usize size, usize alignment, void*) { return detail::aligned_realloc(ptr, size, alignment); },
 				nullptr);
 		}
 
@@ -117,11 +211,11 @@ namespace edge {
 			return stats->alloc_bytes.load() - stats->free_bytes.load();
 		}
 
-		void* malloc(usize size) const noexcept {
+		void* malloc(usize size, usize alignment = alignof(max_align_t)) const noexcept {
 			if (!m_malloc) {
 				return nullptr;
 			}
-			return m_malloc(size, user_data);
+			return m_malloc(size, alignment, user_data);
 		}
 
 		void free(void* ptr) const noexcept {
@@ -131,20 +225,11 @@ namespace edge {
 			return m_free(ptr, user_data);
 		}
 
-		void* realloc(void* ptr, usize size) const noexcept {
+		void* realloc(void* ptr, usize size, usize alignment = alignof(max_align_t)) const noexcept {
 			if (!m_realloc) {
 				return nullptr;
 			}
-			return m_realloc(ptr, size, user_data);
-		}
-
-		void* zeroed(usize nmemb, usize size) const noexcept {
-			usize total = nmemb * size;
-			void* ptr = malloc(total);
-			if (ptr) {
-				memset(ptr, 0, total);
-			}
-			return ptr;
+			return m_realloc(ptr, size, alignment, user_data);
 		}
 
 		char* strdup(const char* str) const noexcept {
@@ -178,21 +263,16 @@ namespace edge {
 
 		template<typename T, typename... Args>
 		T* allocate(Args&&... args) const noexcept {
-			T* ptr = (T*)malloc(sizeof(T));
+			T* ptr = (T*)malloc(sizeof(T), alignof(T));
 			if (!ptr) {
 				return nullptr;
 			}
 
-			//if constexpr (!std::is_trivially_constructible_v<T, Args...>) {
+			//if constexpr (!std::is_constructible_v<T, Args...>) {
 				new (ptr) T(std::forward<Args>(args)...);
 			//}
 
 			return ptr;
-		}
-
-		template<typename T>
-		T* allocate_zeroed(usize count = 1) const noexcept {
-			return (T*)calloc(count, sizeof(T));
 		}
 
 		template<typename T>
@@ -201,12 +281,12 @@ namespace edge {
 				return nullptr;
 			}
 
-			T* ptr = (T*)malloc(sizeof(T) * count);
+			T* ptr = (T*)malloc(sizeof(T) * count, alignof(T));
 			if (!ptr) {
 				return nullptr;
 			}
 
-			//if constexpr (!std::is_trivial_v<T>) {
+			//if constexpr (!std::is_constructable_v<T>) {
 				for (usize i = 0; i < count; ++i) {
 					new (&ptr[i]) T();
 				}
@@ -221,7 +301,7 @@ namespace edge {
 				return;
 			}
 
-			//if constexpr (!std::is_trivial_v<T>) {
+			//if constexpr (!std::destructale_v<T>) {
 				ptr->~T();
 			//}
 
@@ -234,7 +314,7 @@ namespace edge {
 				return;
 			}
 
-			//if constexpr (!std::is_trivially_destructible_v<T>) {
+			//if constexpr (!std::is_destructable_v<T>) {
 				for (usize i = count; i > 0; --i) {
 					ptr[i - 1].~T();
 				}
